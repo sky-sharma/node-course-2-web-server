0833d4104c8da8a2ced6770a7e750f97629d23fd
skysharma <sky.sharma@gmail.com>
2017-08-21 11:25:07 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.use((request, response, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${request.method} ${request.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => { if (err) console.log('Unable to append to server.log');});
+  next();
+});
+
+// app.use((request, response, next) => {
+// response.render('maintenance.hbs');
+//   });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => { return new Date().getFullYear() });
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my Website!'
+  });
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {pageTitle: 'About Page'});
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (request, response) => {
+  // response.send('<h1>Hello Express!</h1>');
+  response.send({errorMessage: 'Unable to handle request.'});
+});
+
+app.listen(3000, () => {console.log('Server is up on port 3000');});
